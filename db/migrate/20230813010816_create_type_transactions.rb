262c5dc902dc739c14e9751d03b0c0ea1ccd40b3
= <=>
2023-08-13 19:47:13 -0300

    Fix(Migration): removed type keyword from TypeTransaction bc it's a reserved ruby word

--- db/migrate/20230813010816_create_type_transactions.rb
+++ db/migrate/20230813010816_create_type_transactions.rb
@@ -1,7 +1,6 @@
 class CreateTypeTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :type_transactions do |t|
-      t.integer :type
       t.string :description
       t.string :nature
       t.string :signal
