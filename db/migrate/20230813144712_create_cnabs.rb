2615c5ab07dbb9a1f7ef0486169e8c7135b44816
= <=>
2023-08-13 19:47:37 -0300

    Feat(migration): created Cnab table

---
+++ db/migrate/20230813144712_create_cnabs.rb
@@ -0,0 +1,16 @@
+class CreateCnabs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cnabs do |t|
+      t.references :type_transaction, null: false, foreign_key: true
+      t.date :date
+      t.decimal :value
+      t.string :cpf
+      t.string :card
+      t.datetime :hour
+      t.string :owner
+      t.string :store_name
+
+      t.timestamps
+    end
+  end
+end
