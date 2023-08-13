b46d0e68e0897471a85df3d307074ef459e81536
= <=>
2023-08-12 22:28:57 -0300

    Feat: added Faker to Rails Helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
 
+require 'faker'
+
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
