3d1f1e3ab2bd050687232e6fcb53b3df8ac1a4aa
= <=>
2023-08-13 19:52:26 -0300

    Chore(Seeds): updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,54 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+TypeTransaction.create(
+  description: 'Débito',
+  nature: 'Entrada',
+  signal: '+'
+)
+
+TypeTransaction.create(
+  description: 'Boleto',
+  nature: 'Saída',
+  signal: '-'
+)
+
+TypeTransaction.create(
+  description: 'Financiamento',
+  nature: 'Saída',
+  signal: '-'
+)
+
+TypeTransaction.create(
+  description: 'Crédito',
+  nature: 'Entrada',
+  signal: '+'
+)
+
+TypeTransaction.create(
+  description: 'Recebimento Empréstimo',
+  nature: 'Entrada',
+  signal: '+'
+)
+
+TypeTransaction.create(
+  description: 'Vendas',
+  nature: 'Entrada',
+  signal: '+'
+)
+
+TypeTransaction.create(
+  description: 'Recebimento TED',
+  nature: 'Entrada',
+  signal: '+'
+)
+
+TypeTransaction.create(
+  description: 'Recebimento DOC',
+  nature: 'Entrada',
+  signal: '+'
+)
+
+TypeTransaction.create(
+  description: 'Aluguel',
+  nature: 'Saída',
+  signal: '-'
+)
+
