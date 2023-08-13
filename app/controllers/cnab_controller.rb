202ea5c1bcbd0f9e6dd935e375eb18c174027649
= <=>
2023-08-13 19:48:19 -0300

    Feat(Cnabs): added a controller for cnabs

---
+++ app/controllers/cnab_controller.rb
@@ -0,0 +1,22 @@
+class CnabController < ApplicationController
+  def index
+    @cnabs = Cnab.all
+    @entries = CnabAnalyticsService.entries
+    @outflows = CnabAnalyticsService.outflows
+    @balance = CnabAnalyticsService.balance
+  end
+
+  def import
+    file = params[:file]
+    CnabImportService.import(file)
+
+    @cnabs = Cnab.all
+  end
+
+  def destroy
+    @cnab = Cnab.find(params[:id])
+    @cnab.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+end
