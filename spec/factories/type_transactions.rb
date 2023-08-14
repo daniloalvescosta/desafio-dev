15944f500570bb5fb91e29f46b76d43cba58e24f
= <=>
2023-08-13 21:33:10 -0300

    Refactor(factorybot): refactored type transaction factory

--- spec/factories/type_transactions.rb
+++ spec/factories/type_transactions.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :type_transaction do
-    type { Faker::Number.unique.between(from: 1, to: 10) } # Change the range as needed
     description { Faker::Lorem.sentence }
     nature { ['income', 'expense'].sample }
     signal { ['+', '-'].sample }
