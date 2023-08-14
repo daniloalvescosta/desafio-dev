613cf48715fe1f5cf3d4d4a74c349f19a3d6786f
= <=>
2023-08-13 21:39:57 -0300

    Refactor(Rubocop): added frozen string comment and whitespaces

--- app/service/cnab_analytics_service.rb
+++ app/service/cnab_analytics_service.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class CnabAnalyticsService < ApplicationService
   def self.entries
-    ids = TypeTransaction.where(nature:'Entrada').ids
+    ids = TypeTransaction.where(nature: 'Entrada').ids
     Cnab.where(type_transaction: ids).sum(:value)
   end
 
   def self.outflows
-    ids = TypeTransaction.where(nature:'Saída').ids
+    ids = TypeTransaction.where(nature: 'Saída').ids
     Cnab.where(type_transaction: ids).sum(:value)
   end
 
