e5e13abf16bc77a4a190500d51074ff83f18cc01
= <=>
2023-08-13 21:34:42 -0300

    Refactor(rspec): removed type from type transaction test suit

--- spec/models/type_transaction_spec.rb
+++ spec/models/type_transaction_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe TypeTransaction, type: :model do
 
   describe 'factory' do
     it { expect(type_transaction).to be_valid }
-    it { expect(type_transaction.type).to be_present }
     it { expect(type_transaction.description).to be_present }
     it { expect(type_transaction.nature).to be_present }
     it { expect(type_transaction.signal).to be_present }
