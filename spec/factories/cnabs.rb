205b3545ee2f5337999fbfb1a065b19f0ba371d0
= <=>
2023-08-13 21:32:46 -0300

    feat(factorybot): added cnab factory

---
+++ spec/factories/cnabs.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :cnab do
+    type_transaction { nil }
+    date { '2021-04-27' }
+    value { '42.80' }
+    cpf { '111.222.333-4' }
+    card { 'Discover' }
+    hour { '09:30:00' }
+    owner { 'Nova Grace' }
+    store_name { 'Starlight Souvenirs' }
+  end
+end
